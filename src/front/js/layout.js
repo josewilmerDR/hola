3c4705aa62ceecf405de36d49d9bbb693eee359a
josewdr <50268140+josewdr@users.noreply.github.com>
2023-10-15 06:53:08 -0600

    first commmit

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -26,7 +26,8 @@ import ProductDetails from "./component/productDetails.jsx";
 const Layout = () => {
   //the basename is used when your project is published in a subdirectory and not in the root of the domain
   // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-  const basename = process.env.BASENAME || "";
+  //const basename = process.env.BASENAME || "";
+  const basename = "/" || "";
 
   return (
     <div>
